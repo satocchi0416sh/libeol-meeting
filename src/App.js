d930c4814227ec05ed2d3ae6852d3c2894ac6cc6
satocchi0416sh <satoyoshi44@gmail.com>
2022-01-10 09:45:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './App.css';
 
 function App() {
+
+  const month = "01";
+  const date = "10";
+  const hour = "10";
+  const minutes = "00";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+      <div class="card transition">
+        <h2 class="transition">Libeolミーティング</h2>
+        <p>開始: {month}月{date}日  {hour}:{minutes}<br />
+          ミーティングID: 959 2140 9152<br />
+          パスコード: X0r0Tf
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div class="cta-container transition"><a href="https://zoom.us/j/95921409152?pwd=bU4xWFlVTmxOb2tvY2FzRU5VNlBFZz09" class="cta" target="_blank">ミーティングに参加</a></div>
+        <div class="card_circle transition"></div>
+      </div>
     </div>
   );
 }
